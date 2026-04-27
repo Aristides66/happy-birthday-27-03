11622c01a7c6ebb85cf1e71e1bf568e6d86d705d
Aristides <aristides.neto3@aluno.ce.gov.br>
2026-04-27 00:03:51 +0000

    feito

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const msg = document.getElementById("mensagem-extra");
+
+    msg.innerText = "Você merece o mundo inteiro, e mesmo assim o mundo ainda seria pouco perto do que você é. Feliz aniversário, meu amor! 💖";
+}
